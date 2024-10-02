8941a58d79e2f0d2239f825c43bbc921a6aaae4b
Oussamahamani <oussamahamani2000@gmail.com>
2024-10-02 10:11:25 -0400

    adding signup component with a form that logs password/email

---
+++ src/SignUp.jsx
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+
+export default function SignUp() {
+
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log("Submit",email,password)
+    }
+  return (
+    <div>
+        <form action="" onSubmit={handleSubmit}>
+            <div>
+
+            <label htmlFor="">Email</label>
+            <input type="text" onChange = {e=>setEmail(e.target.value)}/>
+            </div>
+            <div>
+
+            <label htmlFor="">Password</label>
+            <input type="text" onChange = {e=>setPassword(e.target.value)}/>
+            </div>
+            <button>sign up</button>
+        </form>
+    </div>
+  )
+}
